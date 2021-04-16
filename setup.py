b8c776b1af25b9c535b0cfb68d28d30d36b11db2
aforr <18561906+aforr@users.noreply.github.com>
2021-04-16 12:43:51 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lineageot",
+    version="0.0.1",
+    author="Aden Forrow",
+    author_email="aden.forrow@maths.ox.ac.uk",
+    description="LineageOT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aforr/LineageOT",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
