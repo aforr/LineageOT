f01e78adcdc0cde4ca3845997b5e149d74a849e3
aforr <18561906+aforr@users.noreply.github.com>
2021-10-21 16:26:25 +0100

    explain POT issue 93 test more

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -15,6 +15,10 @@ class Test_Dependencies():
     def test_ot_issue_93(self):
         """
         Testing for the issue described at https://github.com/PythonOT/POT/issues/93
+
+        If this test fails, LineageOT might still run correctly, but some evaluations on synthetic data will be inaccurate.
+
+        Consider using either a different operating system or a corrected version of PythonOT.
         """
         d = ot.emd2([],[],np.ones([292, 3]))
         assert( abs(d-1) < 10**(-8))
